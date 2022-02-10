306d9102856f88c2b818d8d411757d771d356a31
Tushar Kesarwani <tusharkesarwani44@gmail.com>
2022-02-10 17:30:02 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+VANTA.BIRDS({
+    el: document.getElementById("animated_background"),
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    birdSize: 0.50,
+    wingSpan: 17.00,
+    speedLimit: 7.00,
+    separation: 21.00,
+    alignment: 14.00,
+    cohesion: 100.00
+  })
